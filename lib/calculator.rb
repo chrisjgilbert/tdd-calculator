d073aa6b25c784d1244e6d71591bcee9df88d180
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-20 13:37:32 +0000

    Calculator #add initial tests green

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,7 +1,7 @@
 class Calculator
 
   def add(number_1, number_2)
-    7
+    number_1 + number_2
   end
 
 end
--- spec/unit/calculator_spec.rb
+++ spec/unit/calculator_spec.rb
@@ -3,9 +3,14 @@ require 'calculator'
 describe Calculator do
 
   describe "#add" do
-    it "adds two numbers together" do
+    it "adds two positive numbers together" do
       calculator = Calculator.new
       expect(calculator.add(5, 2)).to eq 7
+    end
+
+    it "adds two negative numbers together" do
+      calculator = Calculator.new
+      expect(calculator.add(-1, -2)).to eq -3
     end
   end
 
